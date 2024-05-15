fdf1fdfa33663949a7155bc487591fad38732e32
Ibrhim-Wael <ibrahemashhab@gmail.com>
2024-05-15 09:40:53 +0300

    Proplem2.cpp

---
+++ Proplem2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+    int Nums[5];
+cout <<"Enter 5 numbers"<<endl;
+for(int i=0 ;i<5;i++){
+    cin>>Nums[i];
+    }
+for(int i=4 ;i>=0;i--){
+    cout << Nums[i]<<" ";
+}
+}
